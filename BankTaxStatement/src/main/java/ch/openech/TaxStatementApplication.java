a35f26bb69486634f72f58e04967e008c3d60c57
bruno <bruno.eberhard@pop.ch>
2016-01-16 13:46:50 +0100

    Persistence: only the root entity classes have to be listed in Appl.

--- BankTaxStatement/src/main/java/ch/openech/TaxStatementApplication.java
+++ BankTaxStatement/src/main/java/ch/openech/TaxStatementApplication.java
@@ -11,17 +11,6 @@ import org.minimalj.frontend.action.Action;
 import ch.openech.action.TaxStatementEditor;
 import ch.openech.action.TaxStatementMockAction;
 import ch.openech.action.TaxStatementXmlImport;
-import ch.openech.model.common.Canton;
-import ch.openech.model.common.CountryIdentification;
-import ch.openech.model.tax.BankAccount;
-import ch.openech.model.tax.BankAccountPayment;
-import ch.openech.model.tax.Expense;
-import ch.openech.model.tax.SecurityDepot;
-import ch.openech.model.tax.SecurityPayment;
-import ch.openech.model.tax.SecurityPurchaseDisposition;
-import ch.openech.model.tax.SecuritySecurity;
-import ch.openech.model.tax.SecurityStock;
-import ch.openech.model.tax.SecurityTaxValue;
 import ch.openech.model.tax.TaxStatement;
 
 public class TaxStatementApplication extends Application {
@@ -63,7 +52,6 @@ public class TaxStatementApplication extends Application {
 
 	@Override
 	public Class<?>[] getEntityClasses() {
-		return new Class<?>[] { TaxStatement.class, BankAccount.class, BankAccountPayment.class, Expense.class, SecurityDepot.class,
-				SecuritySecurity.class, SecurityTaxValue.class, SecurityPayment.class, SecurityStock.class, SecurityPurchaseDisposition.class, CountryIdentification.class, Canton.class };
+		return new Class<?>[] { TaxStatement.class };
 	}
 }
