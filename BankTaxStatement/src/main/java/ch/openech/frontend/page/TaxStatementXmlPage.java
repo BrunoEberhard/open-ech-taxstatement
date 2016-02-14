a32ad5ef20387ef65bf404fe110ebc31f8daa264
bruno <bruno.eberhard@pop.ch>
2016-02-14 16:10:24 +0100

    TaxStatementXmlPage: erst verzögert den Inhalt kreiieren

--- BankTaxStatement/src/main/java/ch/openech/frontend/page/TaxStatementXmlPage.java
+++ BankTaxStatement/src/main/java/ch/openech/frontend/page/TaxStatementXmlPage.java
@@ -2,19 +2,34 @@ package ch.openech.frontend.page;
 
 import java.io.StringWriter;
 
-import org.minimalj.frontend.page.HtmlPage;
+import org.minimalj.frontend.Frontend;
+import org.minimalj.frontend.Frontend.IContent;
+import org.minimalj.frontend.page.Page;
 import org.minimalj.util.StringUtils;
 
 import ch.openech.model.tax.TaxStatement;
 import ch.openech.xml.write.EchSchema;
 import ch.openech.xml.write.WriterEch0196;
 
-public class TaxStatementXmlPage extends HtmlPage {
+// HtmlPage cannot be used because it would immediately load all data
+public class TaxStatementXmlPage extends Page {
+	
+	private final TaxStatement taxStatement;
 	
 	public TaxStatementXmlPage(TaxStatement taxStatement) {
-		super(createHtml(taxStatement), "XML");
+		this.taxStatement = taxStatement;
 	}
 
+	@Override
+	public IContent getContent() {
+		return Frontend.getInstance().createHtmlContent(createHtml(taxStatement));
+	}
+	
+	@Override
+	public String getTitle() {
+		return "XML";
+	}
+	
 	private static String createHtml(TaxStatement taxStatement) {
 		StringWriter stringWriter = new StringWriter();
 		WriterEch0196 w = new WriterEch0196(stringWriter, EchSchema.getNamespaceContext(196, "1.0"));
