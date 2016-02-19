ca6d763d4c91721dcf59e7e396c094fe051dc382
bruno <bruno.eberhard@pop.ch>
2016-02-19 08:49:54 +0100

    Writer should write the same if its output is read

---
+++ BankTaxStatement/src/test/java/org/minimalj/test/TaxStatementXmlTest.java
@@ -0,0 +1,26 @@
+package org.minimalj.test;
+
+import org.junit.Test;
+
+import ch.openech.model.tax.TaxStatement;
+import ch.openech.xml.read.StaxEch0196;
+import ch.openech.xml.write.WriterEch0196;
+import junit.framework.Assert;
+
+public class TaxStatementXmlTest {
+
+	@Test
+	public void testXmlConversion() {
+		TaxStatement taxStatementOriginal = new TaxStatement();
+		taxStatementOriginal.mock();
+		
+		String xml1 = WriterEch0196.writeToString(taxStatementOriginal);
+		
+		TaxStatement taxStatementRead = new StaxEch0196().process(xml1);
+		
+		String xml2 = WriterEch0196.writeToString(taxStatementRead);
+		
+		Assert.assertEquals("Writer should write the same after reading its own output", xml1, xml2);
+	}
+	
+}
