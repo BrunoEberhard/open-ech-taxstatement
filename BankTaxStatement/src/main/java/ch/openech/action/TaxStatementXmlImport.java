18a509debd091ce73f9712f16bfe37866a2a31b5
bruno <bruno.eberhard@pop.ch>
2016-03-02 09:54:32 +0100

    TaxStatementXmlImport: statement auch in Backend schreiben

--- BankTaxStatement/src/main/java/ch/openech/action/TaxStatementXmlImport.java
+++ BankTaxStatement/src/main/java/ch/openech/action/TaxStatementXmlImport.java
@@ -3,10 +3,13 @@ package ch.openech.action;
 import java.util.Collections;
 import java.util.List;
 
+import org.minimalj.backend.Backend;
 import org.minimalj.frontend.Frontend;
 import org.minimalj.frontend.action.Action;
+import org.minimalj.model.validation.ValidationMessage;
 
 import ch.openech.frontend.page.TaxStatementPage;
+import ch.openech.model.EchSchemaValidation;
 import ch.openech.model.tax.TaxStatement;
 import ch.openech.xml.read.StaxEch0196;
 
@@ -30,9 +33,17 @@ public class TaxStatementXmlImport extends TaxStatementXmlEditor {
 	}
 
 	@Override
+	protected void validate(XmlValue value, List<ValidationMessage> validationMessages) {
+		String result = EchSchemaValidation.validate(value.xml);
+		if (!EchSchemaValidation.OK.equals(result)) {
+			validationMessages.add(new ValidationMessage(XmlValue.$.xml, result));
+		}		
+	}
+	
+	@Override
 	protected TaxStatement save(XmlValue xmlValue) {
 		TaxStatement taxStatement = new StaxEch0196().process(xmlValue.xml);
-		return taxStatement;
+		return Backend.save(taxStatement);
 	}
 
 	@Override
